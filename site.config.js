2002ad4a014696ee0bb1ad407df72a1ede415284
Ben Edwards <hello@benedfit.com>
2025-01-13 11:38:26 +0000

    feat(meta): change title and description format

--- site.config.js
+++ site.config.js
@@ -7,9 +7,9 @@ const config = {
   url: process.env.NEXT_PUBLIC_SITE_URL,
   name: 'lottadoodles',
   shortName: 'Charlotte',
-  titleTemplate: name => `☆ %s ☆ ${name} ☆`,
-  title: '☆ lottadoodles ☆ animation + game design student ☆',
-  description: '☆ animation + game design student ☆ fantasy + character art ☆',
+  titleTemplate: name => `%s ☆ ${name}`,
+  title: 'lottadoodles ☆ animation + game design student',
+  description: 'animation + game design student ☆ fantasy + character art',
   logo: {
     bitmap: logoBitmap.src,
     vector: logoVector
