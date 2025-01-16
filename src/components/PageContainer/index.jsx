8948ee58da4206aa137daad57990c500551fe1ec
Ben Edwards <hello@benedfit.com>
2025-01-16 16:19:19 +0000

    feat: fonts

--- src/components/PageContainer/index.jsx
+++ src/components/PageContainer/index.jsx
@@ -8,7 +8,20 @@ import Header from '~components/Header'
 
 const PageContainer = ({ meta, children }) => (
   <ThemedPageContainer gutter header={<Header />} footer={<Footer />}>
-    <Meta {...meta} />
+    <Meta
+      {...meta}
+      additionalLinkTags={[
+        ...[{ path: 'cera-round-pro' }].map(
+          ({ path, weight = 'regular', extension = 'woff' }) => ({
+            rel: 'preload',
+            href: `/fonts/${path}/${weight}.${extension}`,
+            as: 'font',
+            type: `font/${extension}`,
+            crossOrigin: 'anonymous'
+          })
+        )
+      ]}
+    />
     {children}
   </ThemedPageContainer>
 )
