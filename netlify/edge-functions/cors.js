8970c7384382abcdb07042ad56df2b06c4824c1c
Ben Edwards <hello@benedfit.com>
2025-02-04 11:15:42 +0000

    chore: debug

--- netlify/edge-functions/cors.js
+++ netlify/edge-functions/cors.js
@@ -1,15 +1,18 @@
 export default async (request, { next }) => {
   const response = await next()
-  const origin = request.headers.get('Origin')
+  // const origin = request.headers.get('Origin')
 
-  if (origin) {
-    response.headers.set('Access-Control-Allow-Origin', origin)
-    response.headers.set('Access-Control-Allow-Credentials', 'true')
-  }
+  // if (origin) {
+  response.headers.set('Access-Control-Allow-Origin', '*')
+  response.headers.set('Access-Control-Allow-Credentials', 'true')
+  // }
 
-  console.log(111, origin)
+  console.log(111, request.path)
 
   return response
 }
 
-export const config = { name: 'Set CORS headers', path: ['/college/*'] }
+export const config = {
+  name: 'Set CORS headers',
+  path: ['/college', '/college/*']
+}
