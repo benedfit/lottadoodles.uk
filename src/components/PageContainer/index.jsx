d70361df0ec4c901100c3e98bacd74809766691e
Ben Edwards <hello@benedfit.com>
2025-06-26 15:52:06 +0100

    chore: optimise backdrop

--- src/components/PageContainer/index.jsx
+++ src/components/PageContainer/index.jsx
@@ -1,30 +1,57 @@
-import { PageContainer as ThemedPageContainer } from '@newhighsco/chipset'
+import {
+  Backdrop,
+  PageContainer as ThemedPageContainer
+} from '@newhighsco/chipset'
 import { Meta } from '@newhighsco/press-start'
+import { getImageProps } from 'next/image'
 import { node, object } from 'prop-types'
 import React from 'react'
 
 import Footer from '~components/Footer'
 import Header from '~components/Header'
+import backdropUrl from '~images/backdrop.png'
 
-const PageContainer = ({ meta, children }) => (
-  <ThemedPageContainer gutter header={<Header />} footer={<Footer />}>
-    <Meta
-      {...meta}
-      additionalLinkTags={[
-        ...[{ path: 'cera-round-pro' }].map(
-          ({ path, weight = 'regular', extension = 'woff' }) => ({
-            rel: 'preload',
-            href: `/fonts/${path}/${weight}.${extension}`,
-            as: 'font',
-            type: `font/${extension}`,
-            crossOrigin: 'anonymous'
-          })
-        )
-      ]}
-    />
-    {children}
-  </ThemedPageContainer>
-)
+import styles from './PageContainer.module.scss'
+
+const PageContainer = ({ meta, children }) => {
+  const {
+    props: { src }
+  } = getImageProps({ src: backdropUrl })
+
+  return (
+    <ThemedPageContainer
+      gutter
+      header={<Header />}
+      footer={
+        <>
+          <Footer />
+          <Backdrop
+            className={styles.backdrop}
+            style={{ backgroundImage: `url(${src})` }}
+          >
+            &nbsp;
+          </Backdrop>
+        </>
+      }
+    >
+      <Meta
+        {...meta}
+        additionalLinkTags={[
+          ...[{ path: 'cera-round-pro' }].map(
+            ({ path, weight = 'regular', extension = 'woff' }) => ({
+              rel: 'preload',
+              href: `/fonts/${path}/${weight}.${extension}`,
+              as: 'font',
+              type: `font/${extension}`,
+              crossOrigin: 'anonymous'
+            })
+          )
+        ]}
+      />
+      {children}
+    </ThemedPageContainer>
+  )
+}
 
 PageContainer.propTypes = {
   meta: object,
