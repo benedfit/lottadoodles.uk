3e2dd42dea20505c618dda3af60d4c3f4a2cd36a
Ben Edwards <hello@benedfit.com>
2025-09-01 16:59:47 +0100

    chore: wip

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -32,25 +32,27 @@ const HomePage = ({ meta }) => (
     />
     {logo?.bitmap && <LogoJsonLd url={url} logo={urlJoin(url, logo.bitmap)} />}
 
-    <Grid flex align="center" valign="middle">
-      <Grid.Item>
-        <Avatar />
-      </Grid.Item>
-      <Grid.Item>
-        <Prose>
-          <h1>
-            <strong>Hello! I'm {shortName}</strong>, an animation and game
-            design student based in the UK.
-          </h1>
-          <p>
-            I create work with a focus on fantasy illustration, portrait art and
-            character design as well as some 3D modelling and animation.
-          </p>
-        </Prose>
-      </Grid.Item>
-    </Grid>
+    <ContentContainer size="desktop">
+      <Grid align="center">
+        <Grid.Item sizes={['tablet-one-third', 'desktop-one-quarter']}>
+          <Avatar />
+        </Grid.Item>
+        <Grid.Item sizes={['tablet-two-thirds', 'desktop-three-quarters']}>
+          <Prose>
+            <h1>
+              <strong>Hello! I'm {shortName}</strong>, an animation and game
+              design student based in the UK.
+            </h1>
+            <p>
+              I create work with a focus on fantasy illustration, portrait art
+              and character design as well as some 3D modelling and animation.
+            </p>
+          </Prose>
+        </Grid.Item>
+      </Grid>
+    </ContentContainer>
     <ContentContainer size="desktopMedium">
-      <Grid>
+      <Grid flex>
         <Grid.Item sizes="tablet-one-third">
           <Card
             href="/portfolio"
@@ -61,14 +63,14 @@ const HomePage = ({ meta }) => (
         <Grid.Item sizes="tablet-one-third">
           <Card
             href="/college"
-            heading={<Button>College work</Button>}
+            heading={<Button variant="secondary">College work</Button>}
             image={{ render: () => <SampleImage />, ratio: '4:3' }}
           />
         </Grid.Item>
         <Grid.Item sizes="tablet-one-third">
           <Card
             href="/commisions"
-            heading={<Button>Commisions</Button>}
+            heading={<Button variant="tertiary">Commisions</Button>}
             image={{ render: () => <SampleImage />, ratio: '4:3' }}
           />
         </Grid.Item>
