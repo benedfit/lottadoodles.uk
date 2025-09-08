193dbd61bf1c05b91eb45928094c16ac1593d75f
Ben Edwards <hello@benedfit.com>
2025-09-08 17:05:52 +0100

    chore: portfiolio wip

--- src/pages/commisions.jsx
+++ src/pages/commisions.jsx
@@ -1,19 +1,58 @@
-import { Prose } from '@newhighsco/chipset'
-import { object } from 'prop-types'
+import { Button, Card, Grid, Prose, VisuallyHidden } from '@newhighsco/chipset'
+import { object, string } from 'prop-types'
 import React from 'react'
 import urlJoin from 'url-join'
 
+import cardVariants from '~components/Card/Card.module.scss'
 import PageContainer from '~components/PageContainer'
+import Section from '~components/Section'
 import config from '~config'
+import image from '~images/avatar.jpg'
 
 const { url } = config
 
-const CommisionsPage = ({ meta }) => (
-  <PageContainer meta={meta} variant="tertiary">
-    <Prose align="center">
+const options = [
+  { name: 'Sketches', description: 'Description', image, disabled: true },
+  { name: 'Thigh Up', description: 'Description', href: '/', image },
+  { name: 'Glam Sheet', description: 'Description', href: '/', image }
+]
+
+const CommisionsPage = ({ meta, variant = 'tertiary' }) => (
+  <PageContainer meta={meta} variant={variant}>
+    <VisuallyHidden>
       <h1>Comissions</h1>
-      <p>Coming soon</p>
-    </Prose>
+    </VisuallyHidden>
+    <Section size="desktopMedium">
+      <Grid>
+        {options.map(({ name, description, href, image, disabled }, index) => (
+          <Grid.Item
+            key={name}
+            style={{ marginTop: index !== 0 ? '1em' : undefined }}
+          >
+            <Grid valign="middle">
+              <Grid.Item sizes="one-third">
+                <Card
+                  href={href}
+                  heading={
+                    <Button disabled={disabled} variant={variant}>
+                      {disabled ? 'Not accepting requests' : 'Contact'}
+                    </Button>
+                  }
+                  image={{ src: image, ratio: '16:9' }}
+                  className={cardVariants[variant]}
+                />
+              </Grid.Item>
+              <Grid.Item sizes="two-thirds">
+                <Prose>
+                  <h2>{name}</h2>
+                  <p>{description}</p>
+                </Prose>
+              </Grid.Item>
+            </Grid>
+          </Grid.Item>
+        ))}
+      </Grid>
+    </Section>
   </PageContainer>
 )
 
@@ -24,6 +63,7 @@ export const getStaticProps = async () => ({
 })
 
 CommisionsPage.propTypes = {
+  variant: string,
   meta: object
 }
 
